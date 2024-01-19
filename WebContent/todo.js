6f6cff67887f7e32aa4f9587054ca8ccdb19f05c
Shourya Tyagi <shouryatyagi042@gmail.com>
2024-01-19 23:09:39 +0530

    Changed styling of page

--- WebContent/todo.js
+++ WebContent/todo.js
@@ -14,18 +14,32 @@ function displayTasks(tasks) {
 
   tasks.forEach((task) => {
     const listItem = document.createElement("li");
+    const taskDiv = document.createElement("div");
+    taskDiv.classList.add("task-item"); // Add a CSS class for styling
+
     const checkbox = document.createElement("input");
+    checkbox.classList.add("completion-tick");
+
     checkbox.type = "checkbox";
     checkbox.checked = task.status;
-    // checkbox.disabled = true;
 
     // Add a "change" event listener to handle checkbox clicks
     checkbox.addEventListener("change", function () {
       updateTaskStatus(task.id, checkbox.checked);
     });
 
-    listItem.appendChild(checkbox);
-    listItem.appendChild(document.createTextNode(task.description));
+    taskDiv.appendChild(checkbox);
+    taskDiv.appendChild(document.createTextNode(task.description));
+
+    // Add a "click" event listener to mark the task as completed
+    taskDiv.addEventListener("click", function () {
+      if (!task.status) {
+        // Only mark as completed if not already completed
+        updateTaskStatus(task.id, true);
+      }
+    });
+
+    listItem.appendChild(taskDiv);
     taskList.appendChild(listItem);
   });
 }
@@ -67,6 +81,16 @@ function updateTaskStatus(taskId, status) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(updateData),
+  })
+    .then((response) => response.json())
+    .then((tasks) => displayTasks(tasks))
+    .catch((error) => console.error("Error:", error));
+}
+
+function deleteCompletedTasks() {
+  // Send a request to the server to delete completed tasks
+  fetch("http://localhost:8080/TasksMaster/tasks", {
+    method: "DELETE",
   })
     .then((response) => response.json())
     .then((tasks) => displayTasks(tasks))
